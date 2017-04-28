f38a2dd0f0e5e6b0d44c6c85edbfe333a1d9b511
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-28 09:35:31 -0700

    add spec to test dictionary word definition returns definition when called. add empty string to previous spec tests as method now required two arguments.

--- lib/KidTionary.rb
+++ lib/KidTionary.rb
@@ -4,7 +4,10 @@ class Words
   attr_accessor :dictionary_word
   attr_accessor :dictionary_word_definition
 
-  def initialize (dictionary_word)
+  def initialize (dictionary_word, dictionary_word_definition)
     self.dictionary_word = dictionary_word
+    self.dictionary_word_definition = dictionary_word_definition
   end
+
+
 end
--- spec/KidTionary_spec.rb
+++ spec/KidTionary_spec.rb
@@ -6,12 +6,17 @@ require "pry"
 
 describe('Words#initialize') do
   it('returns the word inputted') do
-    new_word = Words.new('apple')
+    new_word = Words.new('apple','')
     expect(new_word.dictionary_word).to(eq('apple'))
   end
 
   it('returns the word inputted') do
-    new_word = Words.new('potato')
+    new_word = Words.new('potato','')
     expect(new_word.dictionary_word).to(eq('potato'))
+  end
+
+  it('returns the word inputted') do
+    new_word = Words.new('potato','is a tomato')
+    expect(new_word.dictionary_word_definition).to(eq('is a tomato'))
   end
 end
