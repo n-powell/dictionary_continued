fa42191a47342697d02959a1579d92678ce3b77a
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-28 15:41:08 -0700

    add more integration spec to test words and definitions are storing on specific pages properly.

--- spec/KidTionary_integration_spec.rb
+++ spec/KidTionary_integration_spec.rb
@@ -5,7 +5,10 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the anagram path', {:type => :feature}) do
-  it ('Takes user input number and returns coin results.') do
+  before() do
+    Words.clear
+  end
+  it ('Takes user input word and adds it to words page.') do
     visit('/')
     fill_in('dictionary_word', :with => 'turtle')
     click_button('Add the Word!')
@@ -13,7 +16,7 @@ describe('the anagram path', {:type => :feature}) do
   end
 
 
-  it ('Takes user input number and returns coin results.') do
+  it ('Takes user input word and adds word to this words unique info page.') do
     visit('/')
     fill_in('dictionary_word', :with => 'turtle')
     fill_in('dictionary_word_definition', :with => 'small')
@@ -22,4 +25,23 @@ describe('the anagram path', {:type => :feature}) do
     expect(page).to have_content('turtle')
   end
 
+
+  it ('Takes user input definition and adds definition to correspondin words unique info page.') do
+    visit('/')
+    fill_in('dictionary_word', :with => 'turtle')
+    fill_in('dictionary_word_definition', :with => 'small')
+    click_button('Add the Word!')
+    visit('/word/1')
+    expect(page).to have_content('small')
+  end
+
+
+  it ('Takes user input word and definition and confirms both are on this words unique info page corresponding to its unique id.') do
+    visit('/')
+    fill_in('dictionary_word', :with => 'house')
+    fill_in('dictionary_word_definition', :with => 'beds')
+    click_button('Add the Word!')
+    visit('/word/1')
+    expect(page).to have_content('bed')
+  end
 end
